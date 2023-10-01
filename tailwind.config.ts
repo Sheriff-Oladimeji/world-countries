54dfae19477f2d6f0d0a97a088e431f0c0f5eb45
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-10-01 04:36:11 +0100

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        DarkElem: "hsl(209, 23%, 22%)",
+        DarkBg: "hsl(207, 26%, 17%)",
+        LightText: "hsl(200, 15%, 8%)",
+        LightInput: "hsl(0, 0%, 52%)",
+        LightBg: "hsl(0, 0%, 98%)",
+        DarkText: "hsl(0, 0%, 100%)",
+      },
+      fontFamily: {
+        Nun: ["Nunito Sans", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
