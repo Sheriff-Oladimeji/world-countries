7dccdc5011de37b68edea2663b49535b65b07254
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-10-03 17:09:48 +0100

    fix: remove bugs from countrydetails page

--- src/app/countries/[id]/page.tsx
+++ src/app/countries/[id]/page.tsx
@@ -1,34 +1,9 @@
-"use client"
-import React from "react";
+import React from 'react'
 
-import { useFetchCountry } from "@/hooks/useFetch";
-
-const CountryDetail = ({ params }: { params: { id: string } }) => {
-  const [data, isLoading] = useFetchCountry(params.id);
-
-  
-  if (isLoading) {
-    return (
-      <div className="flex items-center justify-center h-screen  w-full bg-DarkBg ">
-        <div className="loader"></div>
-      </div>
-    );
-  }
-
-  if (!data) {
-    return (
-      <div className="flex items-center justify-center h-screen  w-full bg-DarkBg text-DarkText font-bold text-xl">
-        Country not found
-      </div>
-    );
-  }
-
+const CountryDetails = () => {
   return (
-    <div>
-      <h1>{data?.ccn3}</h1>
-      {/* Render other details... */}
-    </div>
-  );
-};
+    <div>CountryDetails</div>
+  )
+}
 
-export default CountryDetail;
+export default CountryDetails
